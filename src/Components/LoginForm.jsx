89ae05a7c567a42db758fd23e7becefef2a98b80
Bernardas Laucius <bernardaslaucius@Airy-Macbook.local>
2024-01-12 12:08:34 +0200

    handles loading state

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -9,7 +9,9 @@ import { getAuthenticated } from "../store/authActions";
 const LoginForm = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const isError = useSelector((state) => state.auth.error);
+  const { error: isError, loading: isLoading } = useSelector(
+    (state) => state.auth
+  );
   const [usernameError, setUsernameError] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
   const usernameRef = useRef();
@@ -45,46 +47,53 @@ const LoginForm = () => {
   };
 
   return (
-    <form
-      className={styles.content}
-      onSubmit={handleLogin}
-      data-testid={"loginForm"}
-    >
-      <div className={styles.inputContainer}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          name="username"
-          ref={usernameRef}
-          placeholder="username@example.com"
-          className={styles.input}
-          data-testid={"inputUsername"}
-          required
-        />
-        {usernameError && (
-          <p className={styles.inputError}>Please enter a valid username</p>
-        )}
-      </div>
-      <div className={styles.inputContainer}>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          ref={passwordRef}
-          className={styles.input}
-          data-testid={"inputPassword"}
-          required
-        />
-        {passwordError && (
-          <p className={styles.inputError}>Please enter a valid password</p>
-        )}
-      </div>
-      <div className={styles.buttonContainer}>
-        <button className={styles.buttonLogin}>LOGIN</button>
-      </div>
-      {isError && <p className={styles.loginError}>{isError}</p>}
-    </form>
+    <>
+      {" "}
+      {!isLoading ? (
+        <form
+          className={styles.content}
+          onSubmit={handleLogin}
+          data-testid={"loginForm"}
+        >
+          <div className={styles.inputContainer}>
+            <label htmlFor="username">Username</label>
+            <input
+              id="username"
+              name="username"
+              ref={usernameRef}
+              placeholder="username@example.com"
+              className={styles.input}
+              data-testid={"inputUsername"}
+              required
+            />
+            {usernameError && (
+              <p className={styles.inputError}>Please enter a valid username</p>
+            )}
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              ref={passwordRef}
+              className={styles.input}
+              data-testid={"inputPassword"}
+              required
+            />
+            {passwordError && (
+              <p className={styles.inputError}>Please enter a valid password</p>
+            )}
+          </div>
+          <div className={styles.buttonContainer}>
+            <button className={styles.buttonLogin}>LOGIN</button>
+          </div>
+          {isError && <p className={styles.loginError}>{isError}</p>}
+        </form>
+      ) : (
+        <div className={styles.content}>Loading...</div>
+      )}
+    </>
   );
 };
 
