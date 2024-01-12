b0f2c0e0cc7d79d946917fd0a595cd32ec31bee1
Bernardas Laucius <bernardaslaucius@Airy-Macbook.local>
2024-01-12 11:24:40 +0200

    adds fallback screen for unauthorised access

--- src/Components/PatientsList.jsx
+++ src/Components/PatientsList.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import styles from "./PatientsList.module.css";
 import { formatDate, sortByDateOrName } from "../utils/helpers";
 
 const PatientsList = () => {
-  const patientsList = useSelector((state) => state.auth.data.patients);
+  const navigate = useNavigate();
+  const {
+    data: { patients: patientsList },
+    isAuthenticated,
+  } = useSelector((state) => state.auth);
 
   const levels = [
     ...new Set(patientsList.map((patient) => patient.type.slice(0, 1))),
@@ -13,40 +18,58 @@ const PatientsList = () => {
 
   return (
     <div className={styles.content}>
-      <h1 className={styles.heading}>Patients:</h1>
-      <div className={styles.levelContainer}>
-        {levels.map((type) => {
-          const filteredAndSortedPatients = patientsList
-            .filter(
-              (patient) =>
-                patient.is_completed === false &&
-                patient.type.slice(0, 1) === type
-            )
-            .sort(sortByDateOrName);
+      {isAuthenticated ? (
+        <>
+          <h1 className={styles.heading}>Patients:</h1>
+          <div className={styles.levelContainer}>
+            {levels.map((type) => {
+              const filteredAndSortedPatients = patientsList
+                .filter(
+                  (patient) =>
+                    patient.is_completed === false &&
+                    patient.type.slice(0, 1) === type
+                )
+                .sort(sortByDateOrName);
 
-          return (
-            <div key={Math.random()}>
-              <h2 className={styles.level}>{`${type.toUpperCase()}-LEVEL`}</h2>
-              <div className={styles.cardContainer}>
-                {filteredAndSortedPatients.map((patient) => {
-                  return (
-                    <div className={styles.card} key={Math.random()}>
-                      <p>{`Name: ${patient.name}`}</p>
-                      <p>{`Joined: ${formatDate(patient.joined)}`}</p>
-                      <p>{`Last visit: ${formatDate(
-                        patient.last_visit_date
-                      )}`}</p>
-                      <p>{`Completed: ${
-                        patient.is_completed === false ? "no" : "yes"
-                      }`}</p>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })}
-      </div>
+              return (
+                <div key={Math.random()}>
+                  <h2
+                    className={styles.level}
+                  >{`${type.toUpperCase()}-LEVEL`}</h2>
+                  <div className={styles.cardContainer}>
+                    {filteredAndSortedPatients.map((patient) => {
+                      return (
+                        <div className={styles.card} key={Math.random()}>
+                          <p>{`Name: ${patient.name}`}</p>
+                          <p>{`Joined: ${formatDate(patient.joined)}`}</p>
+                          <p>{`Last visit: ${formatDate(
+                            patient.last_visit_date
+                          )}`}</p>
+                          <p>{`Completed: ${
+                            patient.is_completed === false ? "no" : "yes"
+                          }`}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <div className={styles.content}>
+          <h1 className={styles.heading}>
+            Sorry, you have to be authenticated to view patient information
+          </h1>
+          <button
+            className={styles.buttonNavigate}
+            onClick={() => navigate("/")}
+          >
+            Go to Login
+          </button>
+        </div>
+      )}
     </div>
   );
 };
